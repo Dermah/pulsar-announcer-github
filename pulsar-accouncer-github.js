aa928dbb881a047eca31da2b1fba51b505bdeaec
Sputnik Antolovich <inquiry@dermah.com>
2015-03-22 21:39:58 +1100

    Get recent push events from GitHub and make an array that contains users and avatars

---
+++ pulsar-accouncer-github.js
@@ -0,0 +1,36 @@
+var Transmitter = require("pulsar-transmitter");
+var GitHub = require("github");
+
+var t = new Transmitter();
+t.transmit({"PulseName": "lol"});
+
+var github = new GitHub({
+  // required
+  version: "3.0.0",
+  // optional
+  debug: true,
+  protocol: "https",
+  timeout: 5000,
+  headers: {
+    "user-agent": "pulsar-announcer-github" // GitHub is happy with a unique user agent
+  }
+});
+github.events.get({}, function(err, res) {
+  console.log("Response from GitHub had " + res.length + " events");
+
+  var events = [];
+
+  for (var i = 0; i < res.length; i++) {
+    if (res[i].type === "PushEvent") {
+      console.log("Found");
+      events.push({
+        user: res[i].actor.login,
+        avatar: res[i].actor.avatar_url
+      });
+    }
+  };
+
+  console.log(events);
+
+  console.log("There were " + events.length + " events");
+});
